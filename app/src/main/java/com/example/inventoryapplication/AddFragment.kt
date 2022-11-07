029c93f738da2947c6352eb43c76b0012fe91104
SultanSarygulov <itsme.kaan.sarygulov@gmail.com>
2022-11-08 03:37:06 +0600

    Connected fragments with navigation

--- app/src/main/java/com/example/inventoryapplication/AddFragment.kt
+++ app/src/main/java/com/example/inventoryapplication/AddFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.Navigation
 
 class AddFragment : Fragment() {
 
@@ -13,6 +15,9 @@ class AddFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add, container, false)
+
+        val view = inflater.inflate(R.layout.fragment_add, container, false)
+
+        return view
     }
 }
--- app/src/main/java/com/example/inventoryapplication/InventoryFragment.kt
+++ app/src/main/java/com/example/inventoryapplication/InventoryFragment.kt
@@ -5,9 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.Navigation
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class InventoryFragment : Fragment() {
 
@@ -23,6 +26,11 @@ class InventoryFragment : Fragment() {
         val recyclerView: RecyclerView = view.findViewById(R.id.recyclerview)
         recyclerView.layoutManager = GridLayoutManager(this.context, 2)
         recyclerView.adapter = adapter
+
+        val addButton: FloatingActionButton = view.findViewById(R.id.addButton)
+        addButton.setOnClickListener {
+            Navigation.findNavController(view).navigate(R.id.action_inventoryFragment_to_addFragment)
+        }
 
         return view
     }
